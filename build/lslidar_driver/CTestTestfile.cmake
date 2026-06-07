85a6e0bd61146639ae715507121254e8dac83070
Your Name <your_email@example.com>
2026-06-07 20:03:01 +0800

    chore: 删除编译生成的 build/install/log 目录

--- build/lslidar_driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/Desktop/robot_move/my_base_learn/src/lslidar_driver/LSLIDAR_X_ROS2-20240228/src/lslidar_driver
-# Build directory: /home/pi/Desktop/robot_move/build/lslidar_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/lslidar_driver/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/pi/Desktop/robot_move/install/lslidar_msgs/share/lslidar_msgs/package.sh"
